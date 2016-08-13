9665f22101b45d87a446b143978c82003bed2f2b
saint_croos <ALIKINGAMBAV2@GMAIL.COM>
2016-08-13 09:09:12 -0200

    Update addsudo.lua

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -1,12 +1,12 @@
 do
 
---— By @ali_ghoghnoos ;)
+--— @GURDEN_ROBOT ;)
 local function callback(extra, success, result)
   vardump(success)
   vardump(result)
 end
 local function run(msg, matches)
-      local sudo = 100577715 --Put you id Here !
+      local sudo = 234169062 --Put you id Here !
     local addsudo = 'user#id'..sudo
   local chat = get_receiver(msg)
   if is_momod(msg) then  -- you can set it to is_owner(msg) 
